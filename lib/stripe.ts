406d1453a2b2f2c339896ebdd070dfb85fa7ce01
Humberto Cueva <humbertocueva12@gmail.com>
2026-03-28 13:45:51 -0700

    Revert Stripe API version to match installed sdk@13

--- lib/stripe.ts
+++ lib/stripe.ts
@@ -1,7 +1,7 @@
 import Stripe from 'stripe';
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-  apiVersion: '2025-01-27.acacia',
+  apiVersion: '2023-08-16',
 });
 
 export default stripe;
